8f1d3d2ed90221489496c15471846ba77714f784
zjukd00m <zjukd00m@gmail.com>
2023-09-20 23:44:21 -0600

    Add the authentication service and use storage hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import GlobalLayout from "@/app/layout";
 import AuthContextProvider from "@/context/AuthContext/AuthContext";
 import Dashboard from "@/views/dashboard";
+import { Toaster } from "react-hot-toast";
 
 export default function Home() {
   return (
     <GlobalLayout>
       <AuthContextProvider>
+        <Toaster />
         <Dashboard />
       </AuthContextProvider>
     </GlobalLayout>
